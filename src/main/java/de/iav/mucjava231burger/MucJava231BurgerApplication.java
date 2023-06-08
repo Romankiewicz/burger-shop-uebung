393129c6133f833deaaf9ee982dd5170b14c4450
Mathias.Romankiewicz <Mathias.romankiewicz@gmail.com>
2023-06-08 09:18:05 +0200

    big bang

---
+++ src/main/java/de/iav/mucjava231burger/MucJava231BurgerApplication.java
@@ -0,0 +1,15 @@
+package de.iav.mucjava231burger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MucJava231BurgerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MucJava231BurgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/iav/mucjava231burger/MucJava231BurgerApplicationTests.java
@@ -0,0 +1,15 @@
+package de.iav.mucjava231burger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class MucJava231BurgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
